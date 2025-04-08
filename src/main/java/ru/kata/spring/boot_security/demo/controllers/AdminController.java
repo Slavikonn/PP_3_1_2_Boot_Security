6a7db29c1cda5406056d1d006db78852ed5ba6a5
Slavikonn <slavikon132@gmail.com>
2025-04-08 18:34:23 +0300

    create service and controller for Admin entity

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.AdminService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", adminService.getAllUsers());
+        return "admin-page";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/AdminServiceImpl.java
@@ -0,0 +1,21 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    private final UserRepository userRepository;
+
+    public AdminServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
