92cdbde2b108da1ea80b038f5f7fac450cca82bf
Slavikonn <slavikon132@gmail.com>
2025-04-15 20:46:36 +0300

    update user-page for JS

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
@@ -5,8 +5,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.dto.UserDto;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.utils.UserMapper;
 
 @RestController
 @RequestMapping("/api/user")
@@ -18,8 +20,8 @@ public class UserRestController {
     }
 
     @GetMapping
-    public ResponseEntity<User> getCurrentUser(Authentication authentication) {
+    public ResponseEntity<UserDto> getCurrentUser(Authentication authentication) {
         User user = userService.getUserByName(authentication.getName());
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UserMapper.toDto(user));
     }
 }
--- src/main/resources/static/js/page-loader.js
+++ src/main/resources/static/js/page-loader.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadFragment("navbar-container", "/fragments/navbar.html", loadCurrentUser);
-    loadFragment("user-info", "/fragments/user-info.html", loadUserInfo);
+    loadFragment("user-info", "/fragments/tables/user-info.html", loadUserInfo);
 });
